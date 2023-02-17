01a3c016bca5823b9fea9e93812884d1d2bf2f8b
Jannes Claes <jannes.claes@student.ehb.be>
2023-02-17 14:45:21 +0100

    added canvas and drew an orange

--- exercises/1-functions/1-0-pizza.js
+++ exercises/1-functions/1-0-pizza.js
@@ -1,1 +1,38 @@
 "use strict";
+
+window.onresize = drawOrange;
+
+/* drawLine(); */
+drawOrange();
+//maakt mooie tekeningen
+function drawLine() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
+
+	context.fillStyle = "darkblue";
+	context.lineWidth = 5;
+	context.beginPath();
+	context.rect(50, 50, 100, 100);
+	context.fill();
+	context.stroke();
+
+	context.beginPath();
+	context.fillStyle = "yellow";
+	context.ellipse(200, 200, 100, 50, Math.PI / 4, 0, Math.PI * 2);
+	context.fill();
+	context.stroke();
+}
+
+function drawOrange() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
+
+	context.beginPath();
+	context.fillStyle = "orange";
+	context.arc(100, 100, 50, 0, Math.PI * 2);
+	context.fill();
+}

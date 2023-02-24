9ff857db451a46c9983e7e77430b57eb57efee6d
Jannes Claes <jannes.claes@student.ehb.be>
2023-02-24 18:24:49 +0100

    1-6

---
+++ exercises/1-functions/1-6.js
@@ -0,0 +1,39 @@
+"use strict";
+let context;
+
+set_up();
+
+
+drawSky();
+drawSun();
+drawSea();
+
+
+function set_up(){
+    let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	context = canvas.getContext("2d");
+}
+
+function drawSky(){
+    context.fillStyle = "orange";
+    context.beginPath();
+    context.rect(50,50,250,250);
+
+    context.fill();
+}
+
+function drawSun(){
+    context.fillStyle = "yellow";
+    context.beginPath();
+    context.ellipse(175, 225, 75, 50, Math.PI ,0,Math.PI*2);
+    context.fill();
+}
+
+function drawSea(){
+    context.fillStyle = "blue";
+    context.beginPath();
+    context.rect(50, 225, 250, 75);
+    context.fill();
+}

6a117ef24540961b5b0093543d67f989acce0258
Jannes Claes <jannes.claes@student.ehb.be>
2023-03-03 14:18:06 +0100

    3-1-f finished

--- exercises/3-iteration-while/3-iteration-while/3-1-f.js
+++ exercises/3-iteration-while/3-iteration-while/3-1-f.js
@@ -1,20 +1,20 @@
 "use strict";
-import context from "../../scripts/context.js";
+import context from "../../../scripts/context.js";
 
 draw();
 
 function draw() {
-    context.lineWidth = 2;
-    context.strokeStyle = "white";
-    context.fillStyle = "#3498DB";
-    context.fillRect(50, 50, 300, 300);
-    drawDiagonalSquares();
+	context.lineWidth = 2;
+	context.strokeStyle = "white";
+	context.fillStyle = "#3498DB";
+	context.fillRect(50, 50, 300, 300);
+	drawDiagonalSquares();
 }
 
 function drawDiagonalSquares() {
-    let i = ;
-    while () {
-        context.strokeRect(,,,);
-        i = ;
-    }
+	let i = 0;
+	while (i < 7) {
+		context.strokeRect(75 + 25 * i, 75 + 25 * i, 100, 100);
+		i++;
+	}
 }

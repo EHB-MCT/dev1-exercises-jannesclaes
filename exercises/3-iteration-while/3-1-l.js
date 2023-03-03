93a9c22493cda131a24c3f71e9fa106a2f082c6e
Jannes Claes <jannes.claes@student.ehb.be>
2023-03-03 15:35:13 +0100

    3-1-l finished

--- exercises/3-iteration-while/3-1-l.js
+++ exercises/3-iteration-while/3-1-l.js
@@ -5,17 +5,17 @@ import * as Utils from "../../scripts/utils.js";
 draw();
 
 function draw() {
-    context.lineWidth = 2;
-    context.strokeStyle = "white";
-    context.fillStyle = "#3498DB";
-    context.fillRect(50, 50, 300, 300);
-    drawHourGlass();
+	context.lineWidth = 2;
+	context.strokeStyle = "white";
+	context.fillStyle = "#3498DB";
+	context.fillRect(50, 50, 300, 300);
+	drawHourGlass();
 }
 
 function drawHourGlass() {
-    let i = 0;
-    while () {
-        Utils.drawLine(, , , );
-        i++;
-    }
+	let i = 0;
+	while (i <= 30) {
+		Utils.drawLine(50 + i * 10, 50, 350 - 10 * i, 350);
+		i++;
+	}
 }

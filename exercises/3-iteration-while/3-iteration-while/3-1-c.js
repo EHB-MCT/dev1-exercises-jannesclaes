29449ee6ade4ea51b2cd39f6bbe5b6823c1b539c
Jannes Claes <jannes.claes@student.ehb.be>
2023-03-03 14:01:52 +0100

    3-1-c

--- exercises/3-iteration-while/3-iteration-while/3-1-c.js
+++ exercises/3-iteration-while/3-iteration-while/3-1-c.js
@@ -1,23 +1,23 @@
 "use strict";
-import context from "../../scripts/context.js";
-import * as Utils from "../../scripts/utils.js";
+import context from "../../../scripts/context.js";
+import * as Utils from "../../../scripts/utils.js";
 
 draw();
 
 function draw() {
-    context.lineWidth = 2;
-    context.strokeStyle = "white";
-    context.fillStyle = "#3498DB";
-    context.fillRect(50, 50, 300, 300);
-    drawCross();
+	context.lineWidth = 2;
+	context.strokeStyle = "white";
+	context.fillStyle = "#3498DB";
+	context.fillRect(50, 50, 300, 300);
+	drawCross();
 }
 
 function drawCross() {
-    let i = 0;
-    while (i < 5) {
-        let step = ;
-        Utils.drawLine(50, step, 350, step);
-        Utils.drawLine(step, 50, step, 350);
-        i = ;
-    }
+	let i = 1;
+	while (i <= 5) {
+		let step = 125 + i * 25;
+		Utils.drawLine(50, step, 350, step);
+		Utils.drawLine(step, 50, step, 350);
+		i++;
+	}
 }
